6f6d8244e0acf64e099b059e5a1007143893e063
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-28 21:08:21 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,14 +3,7 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-# Write your code here.
-def badge_maker(name)
-  return "Hello, my name is #{name}."
-end
-
-def batch_badge_creator(names)
-  badges = []
-  names.each{|name|}
-  badges << badge_maker(name)
-return badges
+def batch_badge_creator(attendees)
+messages = attendees.collect {|name| "Hello, my name is #{name}."}
+return messages 
 end
