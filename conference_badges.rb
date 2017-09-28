a0bbba867c4764c79ac096611a8eb91198dc9a5e
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-28 21:04:12 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,9 +3,4 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(names)
-  badges = []
-  names.each{|name|}
-  badges << badge_maker(name)
-end
-return badges
+
